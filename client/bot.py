aefbd40e2f97926e5a7eeb984fe8d07c70bc3bfa
Andres <xxdrews@gmail.com>
2020-12-18 19:06:36 +0100

    basic functionality

--- client/bot.py
+++ client/bot.py
@@ -2,24 +2,40 @@ import socket
 import sys
 
 server = "127.0.0.1"
-channel = "#"
+channel = "#hola"
 botnick = "bot"
 
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("Connecting to:"+server)
 irc.connect((server, 6667))
-irc.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :This is a fun  bot!\n")
-irc.send("NICK "+ botnick +"\n")
-irc.send("PRIVMSG nickserv :iNOOPE\r\n")
-irc.send("JOIN "+ channel +"\n")
+irc.send(("USER "+ botnick +" "+ botnick +" "+ botnick +" This is a fun  bot!\n").encode('utf-8'))
+irc.send(("NICK "+ botnick +"\n").encode('utf-8'))
+irc.send(("JOIN "+ channel +"\n").encode('utf-8'))
+irc.send(("PRIVMSG #hola :HELLOWORLD\n").encode('utf-8'))
 
 while 1:
 
-   text=irc.recv(2040)
-   print(text)
-#    if text.find('PING') != -1:
-#       irc.send('PONG ' + text.split() [1] + '\r\n')
-#    if text.find(':!hi') !=-1:
-#       t = text.split(':!hi')
-#       to = t[1].strip()
-#       irc.send('PRIVMSG '+channel+' :Hello '+str(to)+'! \r\n')
+   text=irc.recv(4096)
+   # print("--------------------\n")
+   # print(text.decode('utf-8'))
+   # print("--------------------\n")
+   if text.find('!hello'.encode('utf-8')) != -1:
+      irc.send("PRIVMSG #hola :Hello User \r\n".encode('utf-8'))
+
+   elif text.find('!slap'.encode('utf-8')) != -1:
+      irc.send("PRIVMSG #hola :got Slapped \r\n".encode('utf-8'))
+   
+   elif text.find('PRIVMSG bot :'.encode('utf-8')) != -1:
+      # recipient = text.decode('utf-8').split('PRIVMSG')
+      # print(text.decode('utf-8'))
+      irc.send("PRIVMSG alvar :Random sentence! \r\n".encode('utf-8'))
+
+   if text.find('end'.encode('utf-8')) != -1:
+      print('Connection closed')
+      irc.close()
+      break
+
+   # if text.find(':!hi') !=-1:
+   #    t = text.split(':!hi')
+   #    to = t[1].strip()
+   #    irc.send('PRIVMSG '+channel+' :Hello '+str(to)+'! \r\n')
