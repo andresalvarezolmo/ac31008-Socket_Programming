b5611af981e21f0848729879734bf589c0b06ce4
bkopplin <bjarne.kopplin@pm.me>
2020-12-21 19:40:37 +0100

    channels are basically working

--- server/channel.py
+++ server/channel.py
@@ -1,7 +1,8 @@
+import logging
 class Channel:
     """
     Class to represent an IRC channel.
-    Used to manage clients and send messages
+    Provides functions to manipulate the channel
     """
 
     def __init__(self, name, creator):
@@ -10,10 +11,22 @@ class Channel:
         self.topic = ''
 
     def join(self, client):
-        self.clients.append(client)
+        """
+        add client to the list of joined clients
+        :param client: the client to join this channel
+        :return:
+        """
+        if client not in self.clients:
+            self.clients.append(client)
 
     def client_str(self):
-        clients = [c.nickname + " " for c in self.clients]
+        """
+        return a whitespace separated list of all clients that are in a specific channel
+        :return:
+        """
+        clients = ""
+        for c in self.clients:
+            clients += c.nickname + " "
         return clients
 
     def braodcast(self, message, sender):
@@ -26,4 +39,5 @@ class Channel:
         for c in self.clients:
             if c is sender:
                 continue
+            logging.debug(f"in channel.braodcast: {c.nickname} - {c.socket}")
             c.privmsg(sender.nickname, self.name, message)
