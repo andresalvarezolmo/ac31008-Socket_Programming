5fce113976ad0ccbfe5c7c5d563bac506047d251
alittek <alina.littek@gmail.com>
2020-12-21 16:23:48 +0100

    command line args for host, port, channel amd bot

--- client/b.py
+++ client/b.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 import socket
 import random
+import argparse
+
+
+
+
 
 #the socket we’ll be using to connect and communicate with the IRC server.
  
@@ -11,6 +16,7 @@ server = "127.0.0.1" # Server localhost ipv4
 # server = "::1" # Server localhost ipv6
 # server = "fc00:1337::19" # Actual ipv6 Server that will be used for marking
 
+port = 6667
 channel = "#testing" # Channel
 
 botnick = "BOT" 
@@ -18,12 +24,30 @@ adminname = "ABC" #IRC nickname to send admin commands to the bot
 exitcode = "Bye " + botnick 
 afile = 'facts.txt' #file saving the random facts available
 users = [] #list with all users on the channel
-user = "hana"
+
+
+# command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--hostname", help="enter the IP address of the server", required = False, default = server)
+parser.add_argument("--portnumber", type=int, help="enter the port of the server", required = False, default = port)
+parser.add_argument("--channelname", help="enter the name of the channel", required = False, default = channel)
+parser.add_argument("--botname", help="enter the name of the bot", required = False, default = botnick)
+
+args = parser.parse_args()
+server = args.hostname
+port = args.portnumber
+channel = args.channelname
+botnick = args.botname
+
+
+print(server)
+
 
 try:
-  ircsock.connect((server, 6667)) # Here we connect to the server using the port 6667
+  ircsock.connect((server, port)) # connect to server at port...
 except:
   print("ERROR: Could not connect to server: " + server)
+  quit()
 
 try:
   #send some information to let the server know who we are. 
@@ -31,6 +55,7 @@ try:
   ircsock.send(bytes("NICK "+ botnick +"\r\n", "UTF-8")) # assign the nick to the bot
 except:
   print("ERROR: Could not send to server: " + server)
+  quit()
 
 # function to join a channel, pass name 
 def joinchan(chan):
@@ -39,6 +64,7 @@ def joinchan(chan):
     ircsock.send(bytes("JOIN "+ chan +"\r\n", "UTF-8")) 
   except:
     print("ERROR: Could not join channel: " + chan)
+    quit()
 
   #ircmsg = ""
   #while ircmsg.find("End of /NAMES list.") == -1: #loop to continually check for and receive new info from server until we get a message with ‘End of /NAMES list.’
@@ -53,6 +79,7 @@ def ping():
     ircsock.send(bytes("PONG :pingis\r\n", "UTF-8"))
   except:
     print("ERROR: Could not send pong!")
+    quit()
 
 
 
@@ -62,6 +89,7 @@ def sendmsg(msg, target):
     ircsock.send(bytes("PRIVMSG "+ target +" :"+ msg +"\r\n", "UTF-8"))
   except:
     print("ERROR: Could not send message to: " + target)
+    quit()
 
 
 
@@ -99,6 +127,7 @@ def slapuser(target=channel):
     ircsock.send(bytes("PRIVMSG "+ target +" :"+ message +"\n", "UTF-8"))
   except:
     print("ERROR: Could not slap user: " + usertoslap)
+    quit()
 
 
 
@@ -108,6 +137,7 @@ def random_line(filename):
     lines = open(filename).read().splitlines()
   except IOError:
     print("ERROR: Could not read from file: " + filename)
+    quit()
 
   return random.choice(lines)
 
@@ -125,6 +155,7 @@ def main():
       ircmsg = ircsock.recv(2048).decode("UTF-8")
     except:
       print("ERROR: IRC server information problem!")
+      quit()
     
     #remove any line break characters from the string
     ircmsg = ircmsg.strip('\n\r')
